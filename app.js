32ef6b8c232335ebcd6ade17fcc8d4f0149df1d8
Fiona Ochs <fiona.oaks@gmail.com>
2020-01-16 10:16:50 -0800

    added visibility changes

--- app.js
+++ app.js
@@ -6,14 +6,16 @@ const resetButton = document.getElementById('resetButton');
 const winsDisplay = document.getElementById('wins');
 const losesDisplay = document.getElementById('loses');
 const drawsDisplay = document.getElementById('draws');
+const resultsList = document.getElementById('resultsList');
 
-let numberTries = document.getElementById('triesSpan');
-const triesText = document.getElementById('triesBox');
+//let numberTries = document.getElementById('tries');
+const triesText = document.getElementById('tries');
 const resultsDisplay = document.getElementById('resultsDisplay');
 
 let wins = 0;
 let loses = 0;
 let draws = 0;
+let tries = 3;
 
 shootButton.addEventListener('click', () => {
     const userInput = document.querySelector('input:checked');
@@ -21,42 +23,60 @@ shootButton.addEventListener('click', () => {
     const computerChoice = getRandomThrow();
     const results = checkResults(userChoice, computerChoice);
     triesText.style.visibility = 'visible';
+    triesBox.style.visibility = 'visible';
     console.log('user', userChoice);
     console.log('computer', computerChoice);
     console.log('results', results);
+    console.log(tries);
+
+    resultsList.style.visibility = 'visible';
     
-    if (numberTries === 0){
+    if (tries === 0){
         resetButton.style.visibility = 'visible';
+        shootButton.disabled = true;
     }
-    numberTries--;
+    
 
     if(results === 'win'){
         wins++;
+        
+        triesText.textContent = tries;
         winsDisplay.textContent = wins;
-        resultsDisplay.classList.remove('hidden');
+        resultsDisplay.style.visibility = 'visible';
         resultsDisplay.textContent = 'You win!';
+        tries--;
     } else if (results === 'lose'){
         loses++;
+
+        triesText.textContent = tries;
         losesDisplay.textContent = loses;
-        resultsDisplay.classList.remove('hidden');
+        resultsDisplay.style.visibility = 'visible';
         resultsDisplay.textContent = 'You lose!'
+        tries--;
 //lose
     } else {
         draws++;
+        triesText.textContent = tries;
         drawsDisplay.textContent = draws;
-        resultsDisplay.classList.remove('hidden');
+        resultsDisplay.style.visibility = 'visible';
         resultsDisplay.textContent = 'It\'s a draw!';
+        tries--;
 //draw
     }
 });
 
 resetButton.addEventListener('click', () => {
-    const resetGame = document.querySelector('input:checked');
-
-    let wins = 0;
-    let loses = 0;
-    let draws = 0;
-    let numberTries = 3;
+    tries = 3;
+    resetButton.style.visibility = 'hidden';
+    resultsDisplay.style.visibility = 'hidden';
+    drawsDisplay.textContent = draws;
+    triesText.textContent = tries;
+    shootButton.disabled = false;
+    resultsList.style.visibility = 'hidden';
+    console.log(tries);
+    wins = 0;
+    loses = 0;
+    draws = 0;
 
 });
 
